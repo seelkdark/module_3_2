025bf19f1a571ee1f674a652d2643ffca2ba3e9b
seelk <sedova08111984@gmail.com>
2024-11-10 21:33:42 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,22 @@
-def send_email(message, recipient,sender = "university.help@gmail.com"):
-    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    elif recipient == sender:
+def is_valid_email(email):
+    return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    # Проверка на корректность адресов электронной почты
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+        return
+
+    if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
-    elif sender == "university.help@gmail.com":
+        return
+
+    if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-    elif sender != "university.help@gmail.com":
+    else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-send_email('messagje','university.help@gmail.com','@.com')
-send_email('messagje','university.help@gmail.com')
-send_email('messagje','universi@.com')
-send_email('messagje','universi@.co')
-send_email('messagje','universi.com')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
